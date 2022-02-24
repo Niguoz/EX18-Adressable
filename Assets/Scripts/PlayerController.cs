e482811f018fe35a358061ab9cc50ef38b4d527f
Niguoz <nicola.pellegrini96@tiscali.it>
2022-02-24 13:28:49 +0100

    pa faoi uf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,16 +9,39 @@ public class PlayerController : MonoBehaviour
 {
     // [AssetReferenceUILabelRestriction("baseship")]
     // public AssetReference asset;
-    public string label;
+    public string[] labels;
     public string path;
     AssetReference r;
-    public AssetReferenceSprite sp;
 
-    public AssetReferenceMaterial m;
+
+    /*public AssetReferenceSprite sp;
+public AssetReferenceMaterial m;*/
+
+    [Obsolete]
     void Start()
     {
-        r = new AssetReference(path);
-        r.LoadAssetAsync<Sprite>().Completed += OnAssetComplete;
+        //r = new AssetReference(path);
+        //r.LoadAssetAsync<Sprite>().Completed += OnAssetComplete;
+        Addressables.LoadAssetsAsync<Sprite>(
+            labels,
+            null,
+            Addressables.MergeMode.Intersection).Completed += OnCompleteList;
+
+        Addressables.InstantiateAsync(path).Completed += OnInstComplete;
+    }
+
+    private void OnInstComplete(AsyncOperationHandle<GameObject> obj)
+    {
+       
+    }
+
+    private void OnCompleteList(AsyncOperationHandle<IList<Sprite>> obj)
+    {
+        foreach(var i in obj.Result)
+        {
+            Debug.Log(i);
+        }
+
     }
 
     private void OnAssetComplete(AsyncOperationHandle<Sprite> obj)
@@ -29,6 +52,6 @@ public class PlayerController : MonoBehaviour
 
     void OnDestroy()
     {
-        r.ReleaseAsset();
+       // r.ReleaseAsset();
     }
 }
